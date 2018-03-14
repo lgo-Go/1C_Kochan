1133305aca94a281960b3fc7388d84937fb016e5
Игорь <pupokbomzha@yandex.ru>
2018-03-14 16:17:41 +0300

    задания 3 и 4

---
+++ ex_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("1. hbjdfhbhs\n2. hbjdfhbhs\n3. kfjghdkjfg\n");
+	printf("4. gfjhej\n5. ронгек\n");
+	return 0;
+}
---
+++ ex_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	int x, y;
+	x = 87;
+	y = 15;
+	printf("87 - 15 = %i\n", x - y);
+	return 0;
+}
